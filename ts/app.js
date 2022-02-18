49a4353a976cfa9155cdace3bbff1a40106163af
Thedraxx <thedraxxgames@gmail.com>
2022-02-18 15:55:59 -0300

    Finish Project

---
+++ ts/app.js
@@ -0,0 +1,44 @@
+"use strict";
+exports.__esModule = true;
+var progress = document.getElementById('progress');
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+var circles = document.querySelectorAll('.circle');
+var currentActive = 1;
+next.addEventListener('click', function () {
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update();
+});
+prev.addEventListener('click', function () {
+    currentActive--;
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
+});
+function update() {
+    circles.forEach(function (circle, idx) {
+        console.log(idx);
+        if (idx < currentActive) {
+            circle.classList.add('active');
+        }
+        else {
+            circle.classList.remove('active');
+        }
+    });
+    var actives = document.querySelectorAll('.active');
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+    if (currentActive === 1) {
+        prev.disabled = true;
+    }
+    else if (currentActive === circles.length) {
+        next.disabled = true;
+    }
+    else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
